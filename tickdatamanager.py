24e24297aefff7810729ed8463a6679661c05edc
宋林硕 <songlinshuo@bupt.edu.cn>
2022-04-14 15:01:33 +0800

    从csv导入数据的部分完成

--- tickdatamanager.py
+++ tickdatamanager.py
@@ -52,18 +52,68 @@ class TickDataManager(object):
     def csvinput(self):
         data = pd.read_csv("E:\办公\毕设\DCi1809 - 副本.csv", sep = ",", parse_dates = ["Time"])
         cursor = self.db.cursor()
-        row = data.loc[0]
-        # csv时间格式待解决
-        sql = """
-        INSERT INTO tickdata1(Time, Price, Volume, Amount, OpenInt)
-        VALUES (%s, %s, %s, %s, %s)
-        """
-        para = [row[0], row[1], row[2], row[3], row[4]]
-        cursor.execute(sql,para)
+        for i in range(len(data)):
+            row = data.loc[i]
+            sql = """
+            INSERT INTO tickdata1(
+                Time,
+                Price,
+                Volume,
+                Amount,
+                OpenInt,
+                TotalVol,
+                TotalAmount,
+                Price2,
+                Price3,
+                LastClose,
+                Open,
+                High,
+                Low,
+                SP1,
+                SP2,
+                SP3,
+                SP4,
+                SP5,
+                SV1,
+                SV2,
+                SV3,
+                SV4,
+                SV5,
+                BP1,
+                BP2,
+                BP3,
+                BP4,
+                BP5,
+                BV1,
+                BV2,
+                BV3,
+                BV4,
+                BV5,
+                isBuy)
+            VALUES (
+                %s, %s, %s, %s, %s,
+                %s, %s, %s, %s, %s,
+                %s, %s, %s, %s, %s,
+                %s, %s, %s, %s, %s,
+                %s, %s, %s, %s, %s,
+                %s, %s, %s, %s, %s,
+                %s, %s, %s, %s
+            )
+            """
+            para = list(row)
+            try:
+                # 执行sql语句
+                cursor.execute(sql, para)
+                # 执行sql语句
+                self.db.commit()
+            except:
+                # 发生错误时回滚
+                self.db.rollback()
 
     def closedb(self):
         self.db.close()
 
 dbmanager = TickDataManager()
+dbmanager.createtable()
 dbmanager.csvinput()
 dbmanager.closedb()
