523a30c1629c993ec7f8c85982f0096f1baa490c
bigandbigstone <songlinshuo@bupt.edu.cn>
2022-05-04 14:49:07 +0800

    基本GUI创建

---
+++ UI_main.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from BackTestGUI.Ui_BackTestWindow import Ui_Dialog
+ 
+class mywindow(QtWidgets.QMainWindow,Ui_Dialog):
+  def __init__(self):
+    super(mywindow,self).__init__()
+    self.setupUi(self)
+ 
+if __name__=='__main__':
+  app = QtWidgets.QApplication(sys.argv)
+  window = mywindow()
+  window.show()
+  sys.exit(app.exec_())
