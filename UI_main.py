9e3423fc1b6c1f044b923713b44ed0ac2aab4430
bigandbigstone <songlinshuo@bupt.edu.cn>
2022-05-04 15:29:16 +0800

    简单图形界面框架

--- UI_main.py
+++ UI_main.py
@@ -1,12 +1,12 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from BackTestGUI.Ui_BackTestWindow import Ui_Dialog
+from BackTestGUI.Ui_Mainwindow import Ui_MainWindow
  
-class mywindow(QtWidgets.QMainWindow,Ui_Dialog):
+class mywindow(QtWidgets.QMainWindow,Ui_MainWindow):
   def __init__(self):
     super(mywindow,self).__init__()
     self.setupUi(self)
- 
+    
 if __name__=='__main__':
   app = QtWidgets.QApplication(sys.argv)
   window = mywindow()
